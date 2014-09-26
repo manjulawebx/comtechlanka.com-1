caa2cd69cacecd8138655bcf1476ed4a496f6bd1
manjula <manjulasrinath@Manjulas-MacBook-Pro.local>
2014-09-26 17:03:46 +0530

    Initial commit to test

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php
+if($_SERVER["SERVER_NAME"]=="localhost")
+{
+ 	define("SITE_ROOT","http://localhost/~manjulasrinath/comtechlanka.com/");
+	define("DOCUMENT_ROOT", $_SERVER["DOCUMENT_ROOT"] ."/comtechlanka.com/");
+}else{
+	define("SITE_ROOT","http://www.comtechlanka.com/");
+	define("DOCUMENT_ROOT", $_SERVER["DOCUMENT_ROOT"] ."/");
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Web Designing Sri Lanka, Professional Website Developer in Sri Lanka.</title>
+<meta name="description" content="CWS provide Web Designing service in Sri Lanka. Our web developers are dedicated to develop best Search Engine Optimized websites. We help Sri Lankan businesses to go International thru the internet." />
+<meta name="keywords" content="web designing sri lanka, web design, website developer, web developer sri lanka, sri lanka website design, sri lanka web designer, comtech web solutions" />
+<link rel="shortcut icon" href="favicon.ico" />
+<link href="styles/styles.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3107892-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body>
+<div class="wrapper" id="header-wrapper">
+	<div id="header">
+    	<div id="header-title">
+        	<a href="<?php echo SITE_ROOT; ?>">Comtech Web Solutions</a><br />
+			<span class="small-text"><a href="<?php echo SITE_ROOT; ?>">Website Designing  &amp;  Website Developing in Sri Lanka</a></span>
+        </div>
+        <div id="header-right">
+   	  <strong>Call: (+94)77 326 3191</strong></div>
+        <div class="wrapper" id="navigation">
+        	<ul>
+            	
+               <!-- <li><a href="">Portfolio</a></li>-->
+                <li><a href="#websiteservices">Website Services</a></li>
+                <li><a href="#websitedeveloper">Website Developer</a></li>
+                <li><a href="#webdesigningreates">Website Designing Rates</a></li>
+                <li><a href="#contactwebsitedesigner">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="wrapper" id="main">
+	<div id="content-left">
+    	<div id="home-main-topics" >
+       	  <h1>SEO Friendly Website Developing/Web Design in Sri Lanka</h1>
+          <ul>
+            <li>
+            <strong>Search Engine Optimised Website</strong> Designing &amp; Developing.</li>
+            <li><strong>Small Businesses in Sri Lanka</strong> needs more online Presence.</li>
+            <li>Attractive but <strong>simple and fast website</strong> will improve your sales.</li>
+            <li>Make your business website with a <strong>unique design</strong>.</li>
+          </ul>
+        </div>
+        <div class="content">
+        	<p>COMTECH WEB SOLUTIONS is a Website Designing and Developing Company  in Sri Lanka. We develop your website with professional looking and according  to latest web standards. So your business website, organisation's website or  personal website will be more Search Engine Friendly. And also unique and well  organized web design will make web surfers to clearly understand the message  your deliver via your website.</p>
+<p>Our development criteria include Content Management Systems,  E-Commerce,  and Web Applications using PHP,MySQL, JQuery, Javascript and AJAX technologies..</p>
+<hr />
+          
+          <h1 class="clr"><a name="websiteservices" id="websiteservices">Website Services</a> Sri Lanka</h1>
+            <ul class="ul-list">
+              <li>Web Designing  &amp; Developing</li>
+              <li>Web Hosting</li>
+              <li>Domain Name Registration</li>
+              <li>Search Engine Optimization</li>
+              <li>SEO Consultation</li>
+            </ul>
+          <p>&nbsp;</p>
+<hr />
+          
+          <a name="websitedeveloper" id="websitedeveloper"></a>
+          <h1>Web Designer/ Website Developer, Sri Lanka</h1>
+            <p><img src="images/ManjulaSrinath.jpg" width="91" height="111" alt="Manjula Srinath" style=" float:left; margin-right:10px;" />Manjula Srinath Rathnayaka<br />
+			<span class="small-text">Founder and chief web designer/developer</span></p> 
+            <p style="clear:left;">&nbsp;</p>
+<hr />
+          
+          <p></p>
+          <a name="webdesigningreates" id="webdesigningreates"></a>
+          <h1>Web Designing/Developing Rates for Sri Lankan Websites</h1>
+          <p>We  have flexible website designing/developing rate which clients can discuss about  their requirement and adjust accordingly. </p>
+          <p>1. Standard Website Package:</p>
+          <ul class="ul-list">
+            <li>Unique/ Custom Design</li>
+            <li>Up to 10 web pages (can be vary according to requirement)</li>
+            <li>Contact Form</li>
+            <li>Flash or JavaScript   Banner </li>
+            <li>SEO friendly URL planning   and coding</li>
+            <li>Free SEO Consultation</li>
+            <li>Free maintenance for 1 year</li>
+            <li>Minimum Rate<strong>:</strong> Rs. 35,000/- <span class="small-text" style="color:#093;">(Contact to get a custom quote fit to your requirement.)</span></li>
+          </ul>
+<p>2. E-Commerce Websites:</p>
+<ul class="ul-list">
+  <li>Contact for Quote.</li>
+</ul>
+<p><em>* Domain Name Registration Charges and Web Hosting Charges are <u>NOT included.</u></em></p>
+<hr />
+          <a name="contactwebsitedesigner" id="contactwebsitedesigner"></a>
+          <h2>Contact info.</h2>
+            <p style="font-size:18px; color:#900;">Phone: (+94) 077 326 3191 (Manjula Srinath)</p>
+   	      
+        	<p>Address: <br />
+  <strong>Comtech Web Solutions</strong><br />
+  #173, Yakkala Park,<br />
+        	  Yakkala,<br />
+        	  Sri Lanka.</p>
+        	<p><br />
+        	</p>
+        </div>
+    </div>
+	<div id="content-right">
+   	  <img src="images/website-projects.jpg" width="320" height="293" alt="Website Projects" />
+   	  <h2 class="bg-graybox1">Our Website Clients</h2>
+      	<h3>Tourism</h3>
+        <ul class="ul-list">
+        	<li><a href="http://www.holidaytravelsrilanka.com" target="_blank">Silver Line Travels</a></li>
+		  <li><a href="http://www.holidaystoursrilanka.com/" target="_blank">AJ Travels</a></li>
+		  <li><a href="http://www.srilankantravelsandtours.com/" target="_blank">Walksons Tourist Pvt Ltd</a></li>
+          <li><a href="http://www.pearltourslanka.com/" target="_blank">Pearl Tours Lanka</a></li>
+          <li><a href="http://www.srilankavacationtours.com/" target="_blank">Sri Lanka Vacation Tours</a></li>
+          <li><a href="http://www.visitsrilankaholidays.com/" target="_blank">DJ Travels</a></li>
+          <li><a href="http://www.srilankaforholidays.com/" target="_blank">Dream Travels - Sri Lanka</a></li>
+          <li><a href="http://www.dreamtravelssrilanka.com/" target="_blank">Dream Travels - Sri Lanka (Budget)</a></li>
+          <li><a href="http://www.srilankaholidaydestinations.com/" target="_blank">Authentic Sri Lanka Holidays</a></li>
+          <li><a href="http://www.vacationssrilanka.com/" target="_blank">Vacations Sri Lanka</a></li>
+          
+          <li><a href="http://www.leisureholidayssrilanka.com/" target="_blank">Leisure Holidays</a></li>
+          <li><a href="http://www.visitsrilankatours.com/" target="_blank">Visit Sri Lanka Tours</a></li>
+        </ul>
+        
+        <h3>Corporate</h3>
+    	<ul class="ul-list">
+          <li><a href="http://www.pabromaster.com/" target="_blank">Pabromaster - Decorative Painting &amp; Waterproofing</a></li>
+		  <li><a href="http://www.royalpaint.lk/" target="_blank">Royal Paints (pvt) Ltd</a></li>
+          <li><a href="http://www.lexuslanka.com/" target="_blank">Lexus Trading</a></li>
+          <li><a href="http://www.tiljaycomputerforms.com/" target="_blank">Tiljay Computer Forms</a></li>
+        </ul>
+        
+        <h3>Other</h3>
+        <ul class="ul-list">  
+          <li><a href="http://www.apeweddings.com/" target="_blank">Ape Weddings - Wedding Portal</a></li>
+          <li><a href="http://www.ybsl.lk/" target="_blank">Youth Business Sri Lanka</a></li>
+          
+    	</ul>
+<div style="height:20px;">&nbsp;</div>
+        
+
+
+        <div style="width:234px; margin:10px auto;">
+
+		
+
+    </div>
+        
+    </div>
+    
+</div>
+<div class="wrapper" id="footer">
+  <ul>
+   		<li><a href="#">Website Designing &amp; Developing Rates</a></li> 
+        <li><a href="#">Search Engine Optimization</a></li>
+        <li><a href="#">Contact Website Developer</a></li>
+        <li><a href="#">About Website Developer</a></li>
+    </ul>
+    <p style="border-top:#999 1px dotted; padding-top:10px;">&nbsp;</p>
+<p>&copy; 2004-<?php echo date('Y'); ?>, All Rights Reserved.</p>
+</div>
+
+<!-- AddThis Smart Layers BEGIN -->
+<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-521f6daa300f2335"></script>
+<script type="text/javascript">
+  addthis.layers({
+    'theme' : 'transparent',
+    'share' : {
+      'position' : 'left',
+      'numPreferredServices' : 5
+    }   
+  });
+</script>
+<!-- AddThis Smart Layers END -->
+</body>
+</html>
